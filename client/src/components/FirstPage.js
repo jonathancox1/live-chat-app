ae50058047ec89da1b9bf1086350aed1d2ca5a57
henry lee <djteski@gmail.com>
2020-08-18 15:57:01 -0500

    Working on the login page-2

--- client/src/components/FirstPage.js
+++ client/src/components/FirstPage.js
@@ -2,19 +2,32 @@ import React, { useState, useEffect } from 'react'
 import io from 'socket.io-client'
 import './FirstPage.css'
 
-function FirstPage () {
+const FirstPage = () => {
+    const [name, setName] = useState('')
+    const [roomCode, setRoomCode] = useState('')
+
+    const handleName = (e) => {
+        console.log(e.currentTarget.value)
+        setName(e.currentTarget.value)
+    }
+
+    const handleRoom = (e) => {
+        console.log(e.currentTarget.value)
+        setRoomCode(e.currentTarget.value)
+    }
+
     return (
         <form>
             <div>
                 <h1 className="top-header">Join the live chat!</h1>
             </div>
             <div className="form-group">
-                <label for="exampleInputEmail1">Your name</label>
-                <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                <label>Your name</label>
+                <input type="email" onChange={handleName} className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
             </div>
             <div className="form-group">
-                <label for="exampleInputPassword1">Room code</label>
-                <input type="password" className="form-control" id="exampleInputPassword1"></input>
+                <label>Room code</label>
+                <input type="text" onChange={handleRoom} className="form-control" id="exampleInputPassword1"></input>
             </div>
             <div className="button-container">
                 <button type="submit" className="btn btn-primary">Submit</button>
